0c15818966edd4732d013268988a0731419bb663
Aaron Kling <webgeek1234@gmail.com>
2023-08-29 13:22:44 -0500

    sm7125-common: Remove elf files in copy files check
    New extract files uses packages instead of copy files
    
    Change-Id: I337c07d13bd350958d4808e1693cc6e03753a96c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -17,7 +17,6 @@
 COMMON_PATH := device/samsung/sm7125-common
 
 BUILD_BROKEN_DUP_RULES := true
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # Android Verified Boot
 BOARD_AVB_ENABLE := true
