5a111d8704dc6b390d5fea0713f52df8ea20b14b
koko-07870 <ko282ko82@gmail.com>
2025-01-03 02:15:11 +0530

    sm7125-common: Patch missing libs to pass ELF checks
    Signed-off-by: koko-07870 <ko282ko82@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -56,14 +56,15 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/libsec-ril.so': blob_fixup()
         .binary_regex_replace(b'ril.dds.call.ongoing', b'vendor.calls.slot_id')
         .sig_replace('E1 03 15 AA 08 00 40 F9 E3 03 14 AA 08 09 40 F9', 'E1 03 15 AA 08 00 40 F9 03 00 80 D2 08 09 40 F9'),
-    'vendor/lib/libsensorlistener.so': blob_fixup()
+    ('vendor/lib/libsensorlistener.so', 'vendor/lib64/libsensorlistener.so'): blob_fixup()
         .add_needed('libshim_sensorndkbridge.so'),
     ('vendor/lib64/hw/gatekeeper.mdfpp.so', 'vendor/lib64/libkeymaster_helper.so', 'vendor/lib64/libskeymaster4device.so'): blob_fixup()
         .replace_needed('libcrypto.so', 'libcrypto-v33.so'),
     ('vendor/lib/libwvhidl.so', 'vendor/lib/mediadrm/libwvdrmengine.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
     ('vendor/lib/unihal_main@2.15.so', 'vendor/lib64/unihal_main@2.15.so'): blob_fixup()
-        .add_needed('libui_shim.so'),
+        .add_needed('libui_shim.so')
+        .add_needed('libshim_sensorndkbridge.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
