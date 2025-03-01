b4f540cefd1316622ac7b747c58df97ee9c46d56
koko-07870 <ko282ko82@gmail.com>
2025-03-01 13:39:18 +0530

    Revert "sm7125-common: Migrate mount point creation out of Android.mk"
    This reverts commit 0f3a4bc00f610fad31ddd7b944ef3e660ce3ac10.

--- common.mk
+++ common.mk
@@ -301,13 +301,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
 
-# Partitions
-PRODUCT_PACKAGES += \
-    vendor_bt_firmware_mountpoint \
-    vendor_dsp_mountpoint \
-    vendor_firmware_mnt_mountpoint \
-    vendor_firmware-modem_mountpoint
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.pixel-libperfmgr \
